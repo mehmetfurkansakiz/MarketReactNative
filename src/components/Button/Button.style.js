65e53d6c686d5001f76f9a8e643d3178a298cfa7
mehmetfurkansakiz <62005335+Furkanium@users.noreply.github.com>
2022-11-23 02:42:11 +0300

    Button component created

---
+++ src/components/Button/Button.style.js
@@ -0,0 +1,17 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    padding: 5,
+    margin: 10,
+    paddingLeft: 25,
+    paddingRight: 25,
+    backgroundColor: '#00186f',
+    borderRadius: 10,
+    alignItems: 'center',
+  },
+  title: {
+    color: '#FFFFFF',
+    fontWeight: 'bold',
+  },
+});
