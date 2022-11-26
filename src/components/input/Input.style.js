6bdf31367f5b762502867b17173ee79dd1b3183d
mehmetfurkansakiz <62005335+Furkanium@users.noreply.github.com>
2022-11-26 03:44:54 +0300

    react native icons added

--- src/components/input/Input.style.js
+++ src/components/input/Input.style.js
@@ -2,14 +2,15 @@ import {StyleSheet} from 'react-native';
 
 export default StyleSheet.create({
   container: {
-    padding: 5,
     paddingLeft: 25,
     paddingRight: 25,
     margin: 5,
     backgroundColor: '#FFFFFF',
     borderRadius: 15,
+    flexDirection: 'row',
+    alignItems: 'center',
   },
-  text: {
-    color: 'black',
+  input: {
+    flex: 1,
   },
 });
