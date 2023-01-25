43a814926a229c74de55ce6d595f0aee1f76a151
mehmet furkan sakiz <mehmetfurkansakiz@gmail.com>
2023-01-25 15:00:49 +0300

    AuthProvider added

--- src/Router.js
+++ src/Router.js
@@ -1,45 +1,68 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {useDispatch, useSelector} from 'react-redux';
 
 import Products from './pages/Products/Products';
 import Detail from './pages/Detail/Detail';
 import Login from './pages/Login/Login';
+import Loading from './components/Loading/Loading';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+
 const Stack = createNativeStackNavigator();
 
 const Router = () => {
+  const userSession = useSelector(s => s.user);
+  const isAuthLoading = useSelector(s => s.isAuthLoading);
+  const dispatch = useDispatch();
+
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="LoginPage"
-          component={Login}
-          options={{
-            title: 'Dükgan Login',
-            headerStyle: {backgroundColor: '#00186f'},
-            headerTitleStyle: {color: '#FFFFFF'},
-          }}
-        />
-        <Stack.Screen
-          name="ProductsPage"
-          component={Products}
-          options={{
-            title: 'Dükgan',
-            headerStyle: {backgroundColor: '#00186f'},
-            headerTitleStyle: {color: '#FFFFFF'},
-          }}
-        />
-        <Stack.Screen
-          name="DetailPage"
-          component={Detail}
-          options={{
-            title: 'Detay',
-            headerStyle: {backgroundColor: '#00186f'},
-            headerTitleStyle: {color: '#FFFFFF'},
-            headerTintColor: 'white',
-          }}
-        />
-      </Stack.Navigator>
+      {isAuthLoading ? (
+        <Loading />
+      ) : !userSession ? (
+        <Stack.Navigator>
+          <Stack.Screen
+            name="LoginPage"
+            component={Login}
+            options={{
+              title: 'Dükgan Login',
+              headerStyle: {backgroundColor: '#00186f'},
+              headerTitleStyle: {color: '#FFFFFF'},
+            }}
+          />
+        </Stack.Navigator>
+      ) : (
+        <Stack.Navigator>
+          <Stack.Screen
+            name="ProductsPage"
+            component={Products}
+            options={{
+              title: 'Dükgan',
+              headerStyle: {backgroundColor: '#00186f'},
+              headerTitleStyle: {color: '#FFFFFF'},
+              headerRight: () => (
+                <Icon
+                  name="logout"
+                  size={30}
+                  color="white"
+                  onPress={() => dispatch({type: 'REMOVE_USER'})}
+                />
+              ),
+            }}
+          />
+          <Stack.Screen
+            name="DetailPage"
+            component={Detail}
+            options={{
+              title: 'Detay',
+              headerStyle: {backgroundColor: '#00186f'},
+              headerTitleStyle: {color: '#FFFFFF'},
+              headerTintColor: 'white',
+            }}
+          />
+        </Stack.Navigator>
+      )}
     </NavigationContainer>
   );
 };
