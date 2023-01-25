f4e1152e85c4333c4f3bb63436e8ea15259e320c
mehmet furkan sakiz <mehmetfurkansakiz@gmail.com>
2023-01-25 15:07:15 +0300

    AuthProvider changes

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import {SafeAreaView, View, Text, Image, Alert} from 'react-native';
 import Button from '../../components/Button';
-import Input from '../../components/input/Input';
+import Input from '../../components/input';
 import {Formik} from 'formik';
 import styles from './Login.style';
 import * as Yup from 'yup';
 import usePost from '../../hooks/usePost';
 import {API_AUTH_URL} from '@env';
+import {useDispatch} from 'react-redux';
 
 const ErrorMessagesSchema = Yup.object().shape({
   // username: Yup.string()
@@ -24,6 +25,7 @@ const ErrorMessagesSchema = Yup.object().shape({
 
 const Login = ({navigation}) => {
   const {data, loading, error, post} = usePost();
+  const dispatch = useDispatch();
 
   function handleLogin(values) {
     post(API_AUTH_URL + '/login', values);
@@ -37,7 +39,7 @@ const Login = ({navigation}) => {
     if (data.status === 'Error') {
       Alert.alert('DÜKGAN', 'User not found!');
     } else {
-      navigation.navigate('ProductsPage');
+      dispatch({type: 'SET_USER', payload: {user: JSON.stringify(user)}});
     }
   }
 
@@ -84,3 +86,20 @@ const Login = ({navigation}) => {
 };
 
 export default Login;
+
+const user = {
+  address: {
+    geolocation: {lat: '-37.3159', long: '81.1496'},
+    city: 'kilcoole',
+    street: 'new road',
+    number: 7682,
+    zipcode: '12926-3874',
+  },
+  id: 1,
+  email: 'john@gmail.com',
+  username: 'johnd',
+  password: 'm38rmF$',
+  name: {firstname: 'john', lastname: 'doe'},
+  phone: '1-570-236-7033',
+  __v: 0,
+};
